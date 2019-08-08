510f850309e10af8c600052e5f238729f253cf8b
Fynn Becker <post@fynn.be>
2019-08-08 13:49:45 +0200

    feat: Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'standard'
+  ],
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ]
+}
